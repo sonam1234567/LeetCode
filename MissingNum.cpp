b65cf7537f21de3fcb48440aa85fcc93786251cb
sonam1234567 <sherpasonam.sherpa54@gmail.com>
2022-01-08 16:47:37 -0800

    Create MissingNum.cpp

---
+++ MissingNum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> MissingNumbers;
+        int current;
+        //Mark all the numbers that are present as -ve.
+        for (int i =0;i<nums.size();i++){
+            current = abs(nums[i]);
+            nums[current -1] = - abs(nums[current-1]);
+        }
+        //All the missing numbers are the psoitive numbers index+1. 
+        for(int j=0;j<nums.size();j++){
+            if (nums[j] > 0){
+                MissingNumbers.emplace_back(j+1);
+            }
+        }
+        
+        //Now check and add all the numbers which are missing (as positive).
+        return MissingNumbers;
+    }
+};
